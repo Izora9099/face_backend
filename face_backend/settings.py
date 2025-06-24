2e163b094f99c74fb2ce565936f8ffb83c611234
Izora9099 <ndifonlemuel@gmail.com>
2025-06-24 14:18:02 +0100

    minor stuff

--- face_backend/settings.py
+++ face_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-syu*5%4c&2oot&)($!z7i(hzo+2@4v@ro&f5e93f0#2^2k!5fj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.111', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['192.168.1.111', 'localhost', '127.0.0.1', '0.0.0.0', '172.30.62.139']
 
 VERSION = "1.2.3"
 
@@ -154,6 +154,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "http://192.168.1.111:3000",
+    "http://172.30.62.139:3000",
 ]
 CORS_ALLOW_ALL_ORIGINS = True  # For development only
 CORS_ALLOW_CREDENTIALS = True
